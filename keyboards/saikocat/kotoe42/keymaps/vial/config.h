7ec2c39c049a79df0ee9b5fa53fbc3b4a49d73f6
Hoa <hoameomu@gmail.com>
2022-09-05 22:20:51 +0800

    [kotoe42] add QMK vial config and keymap

---
+++ keyboards/saikocat/kotoe42/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+// Copyright 2022 saikocat (@saikocat)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x42, 0xE6, 0x4B, 0xF1, 0x76, 0x00, 0x7D, 0x9E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define LAYER_STATE_8BIT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
