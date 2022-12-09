8c8c1309489f3eaed67fcbcd830afe9887be1418
Hoa <hoameomu@gmail.com>
2022-12-10 00:49:49 +0800

    [toraneko] force serial driver to vendor for rp2040

--- keyboards/saikocat/toraneko/rev1/rules.mk
+++ keyboards/saikocat/toraneko/rev1/rules.mk
@@ -6,6 +6,7 @@ ifneq ($(filter $(strip $(CONVERT_TO)),kb2040 promicro_rp2040),)
 	MCU = RP2040
 	BOOTLOADER = rp2040
 	LTO_ENABLE = no
+	SERIAL_DRIVER = vendor
 	PICO_INTRINSICS_ENABLED = no
 else
 	MCU = atmega32u4
