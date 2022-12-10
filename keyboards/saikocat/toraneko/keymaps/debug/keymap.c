1e70a735d23b566fd6bd9076201fa173827c65ae
Hoa <hoameomu@gmail.com>
2022-12-10 15:13:10 +0800

    [toraneko] add debug keymap

---
+++ keyboards/saikocat/toraneko/keymaps/debug/keymap.c
@@ -0,0 +1,51 @@
+// Copyright 2022 saikocat (@saikocat)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _DEFAULT = 0,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_DEFAULT] = LAYOUT_split_3x6_3(
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F,                    S(KC_F), S(KC_E), S(KC_D), S(KC_C), S(KC_B), S(KC_A),
+        KC_G, KC_H, KC_I, KC_J, KC_K, KC_L,                    S(KC_L), S(KC_K), S(KC_J), S(KC_I), S(KC_H), S(KC_G),
+        KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R,                    S(KC_R), S(KC_Q), S(KC_P), S(KC_O), S(KC_N), S(KC_M),
+                                KC_S, KC_T, KC_U,     S(KC_U), S(KC_T), S(KC_S)
+    )
+};
+// clang-format on
+
+/* Debug enable */
+void keyboard_post_init_user(void) {
+    debug_enable   = true;
+    debug_matrix   = true;
+    debug_keyboard = true;
+    debug_mouse    = true;
+}
+
+/* Disable internal pullups - from @daskygit */
+#if defined(DEBUG_USER_DISABLE_INTERNAL_PULLUP_RP2040)
+#    include "pico/bootrom.h"
+void i2c_init(void) {
+    static bool is_initialised = false;
+    if (!is_initialised) {
+        is_initialised = true;
+
+        // Try releasing special pins for a short time
+        palSetLineMode(I2C1_SCL_PIN, PAL_MODE_INPUT);
+        palSetLineMode(I2C1_SDA_PIN, PAL_MODE_INPUT);
+
+        chThdSleepMilliseconds(10);
+
+        palSetLineMode(I2C1_SCL_PIN, PAL_MODE_ALTERNATE_I2C | PAL_RP_PAD_SLEWFAST | PAL_RP_PAD_DRIVE4);
+        palSetLineMode(I2C1_SDA_PIN, PAL_MODE_ALTERNATE_I2C | PAL_RP_PAD_SLEWFAST | PAL_RP_PAD_DRIVE4);
+    }
+}
+#endif
+
+/* OLED asymmetrical
+ * - https://gist.github.com/drashna/79d14917f98f07e73071cbb391fcb654
+ */
